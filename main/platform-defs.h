aed4692fd2cd9e5b366c8dda62b5b78512f2ff89
Andrei <andrei.i.ivanov@gmail.com>
2023-07-07 17:02:57 +0900

    Initial commit

--- main/platform-defs.h
+++ main/platform-defs.h
@@ -1,6 +1,6 @@
 #define CONFIG_LOG_MAXIMUM_LEVEL    3
 #define CONFIG_ESP_WIFI_SSID        "ikfia-guest"
-#define CONFIG_ESP_WIFI_PASS        "f95nder9"
+#define CONFIG_ESP_WIFI_PASS        "******"
 #define CONFIG_ESP_MAXIMUM_RETRY    5
 #define CONFIG_SOCK_TARGET_HOST     "84.237.104.128"
 #define CONFIG_UDP_SOCK_PORT        4200
